ab5904e509778456b62fbc9b45ed7fde479ea96c
Addam Hardy <addam.hardy@gmail.com>
2012-07-30 18:50:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tesseract::Application.config.secret_token = '9c1db876402e1d054946449e6befd8562fdb8568520c93d7f6df4e6afe622cd6ddfe2ca3593a85e752b26d306dd5d43004ebecfdcbd81cc789ef7649b7b22f2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tesseract::Application.config.session_store :cookie_store, key: '_tesseract_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tesseract::Application.config.session_store :active_record_store

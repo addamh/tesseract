27d2448a51e31d7d1fc54dfc184474431482dc1b
Addam Hardy <addam.hardy@gmail.com>
2012-07-31 15:51:44 -0500

    Updating auth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "consumer_key", "consumer_secret"
+  provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
+  provider :linkedin, ENV['linkedin_consumer_key'], ENV['linkedin_consumer_secret']
 end

711c2884f94451c05e9b1aecf3708e035c823f05
Addam Hardy <addam.hardy@gmail.com>
2012-08-04 00:51:13 -0500

    Debugging bootstrap js on heroku problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
-//= require bootstrap-dropdown
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-dropdown
 //= require_tree .

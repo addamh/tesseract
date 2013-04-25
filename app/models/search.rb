cdd8ed31cdf4fe1db267d3c88c21cd343038d074
Nathan Hyatt <nhyatt@gmail.com>
2013-04-25 14:45:32 -0500

    Small search updates

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,11 +2,6 @@ class Search < ActiveRecord::Base
   attr_accessible :name, :skill_id
   def users
     @users ||= find_users
-    @users = User.order(:name)
-    @users = users.where("name like ?", "%#{keywords}%") if name.present?
-    @users = users.where(skill_id: params[:id]) # if skill_id.present?
-    @users
-
   end
 
   private
